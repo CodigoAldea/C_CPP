a22a305a846a6a558b725a9f112eb1602a651752
CodigoAldea <dmdndrd@gmail.com>
2022-07-05 01:49:41 -0700

    new

---
+++ c/hello.c
@@ -0,0 +1,19 @@
+/**
+ * @file hello.c
+ * @author dhawal
+ * @brief 
+ *  hello world program
+ * @version 0.1
+ * @date 2022-07-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include<stdio.h>
+int main()
+{
+    /* code */
+    printf("Hello World");
+    return 0;
+}
