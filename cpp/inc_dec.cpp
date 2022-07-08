d2898c0b4e1a01433eb7e2fa295f3ae479d880de
CodigoAldea <dmdndrd@gmail.com>
2022-07-08 17:28:10 +0530

    new

---
+++ cpp/inc_dec.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main() 
+{
+    int a = 10;
+    int b = 8 ; 
+    int c;
+    cout<< --a << endl;
+    cout<< b--;
+    return 0;
+}
---
+++ cpp/strings.cpp
@@ -0,0 +1,13 @@
+// String and Explaination
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a = "Hello";
+    string b = "world";
+    int c = 10;
+    cout<< a[1];
+    return 0;
+}
