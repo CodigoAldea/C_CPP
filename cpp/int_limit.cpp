041a05702d2870a68fe8aa673f93dce9379a141d
CodigoAldea <dmdndrd@gmail.com>
2022-07-08 15:35:27 +0530

    new

---
+++ cpp/int_limit.cpp
@@ -0,0 +1,10 @@
+// integer Limits
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    cout << " The maximum limit of int data type :" << INT_MAX << endl;
+	cout << " The minimum limit of int data type :" << INT_MIN << endl;
+    return 0;
+}
---
+++ cpp/size.cpp
@@ -0,0 +1,8 @@
+// WAP to find the size of datatype .
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "size of char : " << sizeof(char) << "byte";
+    return 0;
+}
